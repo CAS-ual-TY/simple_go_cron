732712e45b64f82a72b365f83a8d12b25536c78a
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2024-06-18 15:07:59 +0200

    Add more tests

--- golang/cron_test.go
+++ golang/cron_test.go
@@ -30,6 +30,7 @@ func TestNow(t *testing.T) {
 	test(true, fmt.Sprintf("%d %d %d %d %d", now.Second(), now.Minute(), now.Hour(), now.Day(), int(now.Month())), now, t)
 	test(true, fmt.Sprintf("%d %d", now.Second(), now.Minute()), now, t)
 	test(true, fmt.Sprintf("* * * * * %d", now.Weekday()), now, t)
+	test(false, fmt.Sprintf("%d %d %d %d %d", now.Second(), now.Minute(), (now.Hour()+1)%24, now.Day(), int(now.Month())), now, t)
 }
 
 func TestOther(t *testing.T) {
@@ -39,4 +40,5 @@ func TestOther(t *testing.T) {
 	test(true, "30-31", date, t)
 	test(true, "10,30-31,20", date, t)
 	test(false, "10,20 *", date, t)
+	test(false, "* * 22", date, t)
 }
